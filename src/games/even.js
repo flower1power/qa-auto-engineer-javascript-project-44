8c47675b973794cae8adb7c9f698418b6ea209c3
Виктор Лобынцев <sibfight@mail.ru>
2023-12-11 20:00:20 +0300

    fix even

--- src/games/even.js
+++ src/games/even.js
@@ -3,12 +3,8 @@ import { getRandomInt } from '../index.js';
 export const getQuestionWithAnswer = () => {
   const MIN_NUMBER = 1;
   const MAX_NUMBER = 100;
-  let answer;
   const question = getRandomInt(MIN_NUMBER, MAX_NUMBER);
-  if (question % 2 === 0) {
-    answer = 'yes';
-  }
-  answer = 'no';
+  const answer = (question % 2 === 0) ? 'yes' : 'no';
 
   return { question, answer };
 };
